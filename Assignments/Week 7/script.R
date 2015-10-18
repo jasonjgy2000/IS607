bfa2398196429afa4687f142c13db98989f71c29
jasonjgy2000 <bigjgy2008@gmail.com>
2015-10-17 22:46:12 -0400

    Final

--- Assignments/Week 7/script.R
+++ Assignments/Week 7/script.R
@@ -1,5 +1,6 @@
 library(XML)
 library(RJSONIO)
+install.packages("RJSONIO")
 b <- readHTMLTable("books.html")
 View(b)
 
@@ -11,22 +12,28 @@ xmlSize(xmlRoot(x))
 root[["book"]][[1]][[1]]
 xDFrame <- xmlToDataFrame(root)
 View(xDFrame)
+
 library(stringr)
 library(plyr)
 install.packages("RJSONIO")
 isValidJSON("books.json")
 data_set <- fromJSON("books.json")
-data_set.vec <- unlist(t, recursive = TRUE, use.names = TRUE)
-t.vec
-t.vec[str_detect(names(t.vec), "books.Title")]
-sapply(t.vec[[1]], "[[", "books.Year_Published")
-t[[1]][[1]][["Title"]]
-str(sapply(t[[1]], "[[", "Year_Published"))
+str(data_set.vec)
+data_set.vec <- unlist(data_set, recursive = TRUE, use.names = TRUE)
+data_set.unlist <- sapply(data_set[[1]], unlist) 
+data_set.vec[str_detect(names(data_set.vec), "Title")]
+
+t <- do.call("rbind",data_set.unlist)
+View(t)
+
+sapply(data_set.vec[[1]], "[[", "books.Year_Published")
+data_set[[1]][[1]][["Title"]]
+sapply(data_set[[1]], "[[", "Year_Published")
 data_set.unlist <- sapply(t[[1]],unlist)
 str(t.unlist)
 
 data_set.df <- do.call("rbind.fill",lapply(lapply(data_set.unlist, t), data.frame,stringsAsFactors=FALSE))
 
 data_set.df <- do.call("rbind", lapply(data_set, data.frame,stringsAsFactors = FALSE))
-View(data_set.df)
+View(data_set)
 
